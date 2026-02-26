c863cb76920746afc1ffa003f3268b29866ad91d
newcodesubed <subedshahxi@gmail.com>
2026-02-26 23:13:23 +0545

    feat(db): add db connection

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-
+import mongoose from "mongoose";
+import 'dotenv/config';
+import { configDotenv } from "dotenv";
+import { env } from "process";
 const app = express();
 
 app.use(cors({
@@ -20,3 +23,9 @@ const server=http.createServer(app);
 server.listen(8080,()=>{
     console.log("Server running on http://localhost:8080/");
 })
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on('connected', () => console.log('MongoDB connected'));
+mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected'));
+mongoose.connection.on('error', (error: Error) => console.log(error));
