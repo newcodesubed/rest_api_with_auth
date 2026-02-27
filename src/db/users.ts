0699b82b3faef78f821daefbcb401d51525e3004
newcodesubed <subedshahxi@gmail.com>
2026-02-27 13:58:21 +0545

    feat(db): add schema and export model

---
+++ src/db/users.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+// create schema
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+// export model 
+export const UserModel = mongoose.model('User', UserSchema);
