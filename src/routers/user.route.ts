786bf45e6f20b3d8fe756d93e7cf67de4f29ddaf
newcodesubed <subedshahxi@gmail.com>
2026-03-01 16:14:59 +0545

    feat(route): add delete route

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,7 +1,8 @@
-import { getAllUsers } from "../controllers/users.controller";
+import { deleteUser, getAllUsers } from "../controllers/users.controller";
 import express from "express";
 import { isAuthenticated } from "../middlewares/index.middleware";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
+  router.delete('/users/:id', deleteUser);
 };
