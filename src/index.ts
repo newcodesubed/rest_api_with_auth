9468c81ab16d29113320af59fbab528ea4d63b0c
newcodesubed <subedshahxi@gmail.com>
2026-03-01 13:48:10 +0545

    feat(db): add function for repository

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,14 @@ server.listen(8080,()=>{
 })
 
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGO_URL);
-mongoose.connection.on('connected', () => console.log('MongoDB connected'));
-mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected'));
-mongoose.connection.on('error', (error: Error) => console.log(error));
+mongoose.connect(process.env.MONGO_URL);mongoose.connection.on("connected", () => {
+  console.log("Mongo connected");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Mongo error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.warn("Mongo disconnected");
+});
