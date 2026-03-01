22c03156c18403399799e0292e2531bb269a4c95
newcodesubed <subedshahxi@gmail.com>
2026-03-01 22:18:25 +0545

    feat(route): add updateUser function to user routes

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,8 +1,9 @@
-import { deleteUser, getAllUsers } from "../controllers/users.controller";
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users.controller";
 import express from "express";
 import { isAuthenticated, isOwner } from "../middlewares/index.middleware";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
   router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
+  router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
 };
