aac59e053d065021164edebc79952946ec4dd8e8
newcodesubed <subedshahxi@gmail.com>
2026-03-01 16:02:00 +0545

    feat: add middle ware to user route

--- src/routers/index.route.ts
+++ src/routers/index.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import authenticationRoute from "./authentication.route";
+import userRoute from "./user.route";
 
 const router = express.Router();
 
 export default (): express.Router => {
   authenticationRoute(router);
+  userRoute(router);
   return router;
 };
--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,6 +1,7 @@
 import { getAllUsers } from "../controllers/users.controller";
 import express from "express";
+import { isAuthenticated } from "../middlewares/index.middleware";
 
-export default (router: express.Router)=>{
-    router.get('/users', getAllUsers);
-}
+export default (router: express.Router) => {
+  router.get("/users", isAuthenticated, getAllUsers);
+};
