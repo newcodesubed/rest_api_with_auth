7560ff0497dc9fd5ffb0cfa84200ca8336c915dd
newcodesubed <subedshahxi@gmail.com>
2026-03-01 15:48:53 +0545

    feat(middleware): add authentication mddleware

---
+++ src/middlewares/index.middleware.ts
@@ -0,0 +1,28 @@
+import { getUserBySessionToken } from "../db/users";
+import { Request, Response, NextFunction } from "express";
+import { get, merge } from "lodash";
+
+export const isAuthenticated = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const sessionToken = req.cookies["SUBED-AUTH"];
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
